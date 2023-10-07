babb925803ca65b14ae2e753d7763ca5adf2764d
Wenhao Qiu <wenhaoq@hawaii.edu>
2023-10-06 22:51:05 -1000

    Course list and course card complete

---
+++ app/client/src/components/CourseCard.jsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  Button,
+  Typography,
+} from "@mui/material";
+
+const BasicCard = () => {
+  return (
+    <Card variant="outlined" sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          Semester Section
+        </Typography>
+        <Typography variant="h5" component="div">
+          Course Title
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          Instructor
+        </Typography>
+        <Typography variant="body2">
+          Description of course
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Enter</Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default BasicCard;
--- app/client/src/pages/CourseList.jsx
+++ app/client/src/pages/CourseList.jsx
@@ -1,7 +1,79 @@
+import * as React from "react";
+import {
+  Container,
+  CssBaseline,
+  Box,
+  Typography,
+  TextField,
+  Grid,
+  AppBar,
+  Toolbar,
+  IconButton,
+} from "@mui/material";
+import { AccountCircle, Settings } from "@mui/icons-material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CourseCard from "../components/CourseCard";
+const defaultTheme = createTheme();
+
 const CourseList = () => {
-    return(
-        <h1> Test </h1>
-    );
-}
+  const courses = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <CssBaseline component="main">
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Menu
+            </Typography>
+            <IconButton aria-controls="menu-appbar" color="inherit" aria-label="user">
+              <AccountCircle />
+            </IconButton>
+            <IconButton aria-controls="menu-appbar" color="inherit" aria-label="setting">
+              <Settings />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <Container>
+          <Box
+            sx={{
+              marginTop: 5,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Typography component={"h1"} variant={"h5"}>
+              Course List
+            </Typography>
+          </Box>
+          <TextField
+            margin="normal"
+            fullWidth
+            id="course"
+            label="Course name"
+            name="course"
+            autoComplete="course"
+            autoFocus
+          />
+          <Grid
+            container
+            rowSpacing={1}
+            columnSpacing={2}
+            sx={{
+              marginTop: 5,
+            }}
+          >
+            {courses.map((c) => (
+              <Grid item>
+                <CourseCard key={c} />
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </CssBaseline>
+    </ThemeProvider>
+  );
+};
 
 export default CourseList;
