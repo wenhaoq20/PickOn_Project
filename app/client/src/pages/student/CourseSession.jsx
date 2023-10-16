27a92b6d330618df070fc203b85a781af527166a
Wenhao Qiu <wenhaoq@hawaii.edu>
2023-10-15 21:04:34 -1000

    Update CourseSession.jsx

--- app/client/src/pages/student/CourseSession.jsx
+++ app/client/src/pages/student/CourseSession.jsx
@@ -18,7 +18,6 @@ import StudentGameScores from "../../components/student/StudentGameScores";
 import StudentGroup from "../../components/student/StudentGroup";
 import StudentAnonymous from "../../components/student/StudentAnonymous";
 import InstructorAnonymous from "../../components/instructor/InstructorAnonymous";
-import StudentAnonymous from "../../components/student/StudentAnonymous";
 
 const defaultTheme = createTheme();
 
