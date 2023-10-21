2355689bf82360dd8cd2d857cf3805a1b81e3251
Wenhao Qiu <wenhaoq@hawaii.edu>
2023-10-20 16:01:40 -1000

    Added InstructorPickOn to the session manager

--- app/client/src/components/instructor/InstructorPickOn.jsx
+++ app/client/src/components/instructor/InstructorPickOn.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import {
-  Box, Button,
-  Container, CssBaseline, Grid, List, ListItem, ListItemText, TextField, Typography
+  Box,
+  Button,
+  Container,
+  Grid,
+  List,
+  ListItem,
+  ListItemText,
+  TextField,
+  Typography,
 } from "@mui/material";
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import CasinoIcon from '@mui/icons-material/Casino';
-
-const defaultTheme = createTheme();
+import CasinoIcon from "@mui/icons-material/Casino";
 
 const students = [
   { name: "Alice" },
@@ -16,58 +20,65 @@ const students = [
   { name: "Eve" },
 ];
 
-const InstructorGroup = () => {
+const InstructorGroup = ({ onButtonClick }) => {
   return (
-      <ThemeProvider theme={defaultTheme}>
-        <CssBaseline />
-        <Container>
-          <Box
-              sx={{
-                marginTop: 3,
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-              }}
-          >
-            <Typography variant="h3">PickOn Mode</Typography>
+    <Container>
+      <Button onClick={onButtonClick}> Back </Button>
+      <Box
+        sx={{
+          marginTop: 3,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h3">PickOn Mode</Typography>
+      </Box>
+      <Grid container spacing={2} sx={{ mt: 2 }}>
+        <Grid item xs={5} textAlign="center">
+          <Typography variant="h4">Student List</Typography>
+          <Box sx={{ boxShadow: 2 }}>
+            <List>
+              {students.map((member) => (
+                <ListItem>
+                  <ListItemText primary={member.name} />
+                </ListItem>
+              ))}
+            </List>
           </Box>
-          <Grid container spacing={2} sx={{mt: 2}}>
-            <Grid item xs={5} textAlign='center'>
-              <Typography variant='h4'>Student List</Typography>
-              <Box sx={{ boxShadow: 2 }}>
-                <List>
-                  {students.map((member) => (
-                      <ListItem>
-                        <ListItemText primary={member.name}/>
-                      </ListItem>
-                  ))}
-                </List>
-              </Box>
-            </Grid>
-            <Grid item xs={2}></Grid>
-            <Grid item xs={5} textAlign='center' sx={{mt: 5}}>
-              <TextField
-                  id="outlined-multiline-static"
-                  label="Question"
-                  multiline
-                  rows={6}
-                  fullWidth
-                  defaultValue="Enter the question here"
-              />
-              <Button variant="contained" sx={{mt: 2}}>Submit</Button>
-              <Container/>
-              <Button variant="contained" sx={{mt: 8}} size='large' startIcon={<CasinoIcon/>}>PickOn</Button>
-              <TextField
-                  id="outlined-multiline-static"
-                  label="Picked Student"
-                  fullWidth
-                  defaultValue="No current student"
-                  sx={{mt: 1}}
-              />
-            </Grid>
-          </Grid>
-        </Container>
-      </ThemeProvider>
+        </Grid>
+        <Grid item xs={2}></Grid>
+        <Grid item xs={5} textAlign="center" sx={{ mt: 5 }}>
+          <TextField
+            id="outlined-multiline-static"
+            label="Question"
+            multiline
+            rows={6}
+            fullWidth
+            defaultValue="Enter the question here"
+          />
+          <Button variant="contained" sx={{ mt: 2 }}>
+            Submit
+          </Button>
+          <Container />
+          <Button
+            variant="contained"
+            sx={{ mt: 8 }}
+            size="large"
+            startIcon={<CasinoIcon />}
+          >
+            PickOn
+          </Button>
+          <TextField
+            id="outlined-multiline-static"
+            label="Picked Student"
+            fullWidth
+            defaultValue="No current student"
+            sx={{ mt: 1 }}
+          />
+        </Grid>
+      </Grid>
+    </Container>
   );
 };
 
